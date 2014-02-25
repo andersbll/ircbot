b6a7e7bee5263779db7a4945865f461697c59ec0
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2014-02-25 23:34:44 +0100

    initial code

---
+++ ircbot.py
@@ -0,0 +1,60 @@
+import irc.bot
+import irc.strings
+from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
+
+
+class Command():
+    name = None
+    def message(self, line, bot):
+        raise NotImplementedError()
+
+class Coffee(Command):
+    name = 'kaffe'
+    def message(self, line, c, bot):
+        c.notice("Så' der kaf'")
+
+class IRCBot(irc.bot.SingleServerIRCBot):
+    def __init__(self, server, port, channel, nickname):
+        super().__init__([(server, port)], nickname, nickname)
+        self.channel = channel
+
+    def on_nicknameinuse(self, c, e):
+        c.nick(c.get_nickname() + "_")
+
+    def on_welcome(self, c, e):
+        c.join(self.channel)
+
+    def on_privmsg(self, c, e):
+        self.do_command(e, e.arguments[0])
+
+    def on_pubmsg(self, c, e):
+        a = e.arguments[0].split(":", 1)
+        if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
+            self.do_command(e, a[1].strip())
+        return
+
+    def do_command(self, e, cmd):
+        sort(meh)
+        nick = e.source.nick
+        c = self.connection
+
+        if cmd == "disconnect":
+            self.disconnect()
+        elif cmd == "die":
+            self.die()
+        elif cmd == "stats":
+            for chname, chobj in self.channels.items():
+                c.notice(nick, "--- Channel statistics ---")
+                c.notice(nick, "Channel: " + chname)
+                users = sorted(chobj.users())
+                c.notice(nick, "Users: " + ", ".join(users))
+                opers = sorted(chobj.opers())
+                c.notice(nick, "Opers: " + ", ".join(opers))
+                voiced = sorted(chobj.voiced())
+                c.notice(nick, "Voiced: " + ", ".join(voiced))
+        else:
+            c.notice(nick, "Not understood: " + cmd)
+    
+    def start(self):
+        super().start()
+        return 0
---
+++ run.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import sys
+import logging
+import signal
+import argparse
+from ircbot import IRCBot
+
+description = 'IRC bot'
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument('-s', '--server', type=str, default='chat.freenode.org',
+                    help='Server address.')
+parser.add_argument('-P', '--port', type=str, default=6667,
+                    help='Server port.')
+parser.add_argument('-c', '--channel', type=str, required=True,
+                    help='Channel to join.')
+parser.add_argument('-n', '--nick', type=str, required=True,
+                    help='Bot nickname.')
+parser.add_argument('-p', '--password', type=str,
+                    help='Bot password.')
+parser.add_argument('-v', '--verbosity', type=int, default=10,
+                    help='Verbosity level.')
+args = parser.parse_args()
+
+
+def run():
+    retval = -1
+    while retval != 0:
+        bot = IRCBot(args.server, args.port, args.channel, args.nick)
+
+        def handler(signum=None, frame=None):
+            bot.disconnect('Process shutdown.')
+            sys.exit(0)
+        for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGQUIT]:
+            signal.signal(sig, handler)
+
+        try:
+            retval = bot.start()
+        except Exception as e:
+            logging.exception('Exception occurred, restarting.')
+            if retval != 0 and bot.connection.is_connected():
+                bot.disconnect('Exception occurred, restarting.')
+
+
+if __name__ == '__main__':
+    run()
