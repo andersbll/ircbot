fca237fb9157ea859a700b43745280dc7430a3be
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2014-02-26 08:48:33 +0100

    verbosity level is now handled

--- run.py
+++ run.py
@@ -7,24 +7,35 @@ import signal
 import argparse
 from ircbot import IRCBot
 
-description = 'IRC bot'
-parser = argparse.ArgumentParser(description=description)
-parser.add_argument('-s', '--server', type=str, default='chat.freenode.org',
-                    help='Server address.')
-parser.add_argument('-P', '--port', type=str, default=6667,
-                    help='Server port.')
-parser.add_argument('-c', '--channel', type=str, required=True,
-                    help='Channel to join.')
-parser.add_argument('-n', '--nick', type=str, required=True,
-                    help='Bot nickname.')
-parser.add_argument('-p', '--password', type=str,
-                    help='Bot password.')
-parser.add_argument('-v', '--verbosity', type=int, default=10,
-                    help='Verbosity level.')
-args = parser.parse_args()
+
+def parse_args():
+    description = 'IRC bot'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-s', '--server', type=str, default='chat.freenode.org',
+                        help='Server address.')
+    parser.add_argument('-P', '--port', type=str, default=6667,
+                        help='Server port.')
+    parser.add_argument('-c', '--channel', type=str, required=True,
+                        help='Channel to join.')
+    parser.add_argument('-n', '--nick', type=str, required=True,
+                        help='Bot nickname.')
+    parser.add_argument('-p', '--password', type=str,
+                        help='Bot password.')
+    parser.add_argument('-v', '--verbosity', type=str,
+                        choices=['errors', 'bot', 'debug', 'dump'],
+                        default='bot', help='Verbosity level.')
+    return parser.parse_args()
 
 
 def run():
+    args = parse_args()
+    verbosity_levels = {
+        'errors': logging.WARNING,
+        'bot': logging.INFO,
+        'debug': logging.DEBUG,
+        'dump': logging.NOTSET,
+    }
+    logging.basicConfig(level=verbosity_levels[args.verbosity])
     retval = -1
     while retval != 0:
         bot = IRCBot(args.server, args.port, args.channel, args.nick)
